0ff3c230bdf8aabcbcfaf7a52fef9d0d9e94a7ff
Martín Morente Vargas <martin.dev45@gmail.com>
2024-11-12 18:27:54 +0100

    Update Print bv

--- ft_printnb.c
+++ ft_printnb.c
@@ -6,31 +6,28 @@
 /*   By: mmorente <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 12:52:41 by mmorente          #+#    #+#             */
-/*   Updated: 2024/11/12 08:58:32 by martin           ###   ########.fr       */
+/*   Updated: 2024/11/12 18:27:25 by martin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "ft_printf.h"
 
-int		ft_printnb (int	n)
+int		ft_printnb (int	n, int counter)
 {
-	int	counter;
-
-	counter = 0;
-	if(nb == INT_MIN)
+	if(n == INT_MIN)
 	{
-		write(1,'INT_MIN',11);
-		counter = 11;
-		return (counter);
+		
 	}
-	if (nb < 0 && nb != INT_MIN)
+	if (n < 0 && n != INT_MIN)
 	{
 		write(1,'-',1);
 		counter++;
 	}
-	if(nb > 9)
-	{
-		
-	}
+	if(n > 9)
+		ft_printnb((nb / 10), counter++)
+	nb = nb + '0';
+	write(1,&nb,1);
+	counter++;
+	return (counter);	
 }
