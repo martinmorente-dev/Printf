604984fb56820e1d3049b1a36695c3d64439a122
Martín Morente Vargas <martin.dev45@gmail.com>
2024-11-12 09:25:22 +0100

    print_nb update

--- ft_printnb.c
+++ ft_printnb.c
@@ -6,7 +6,7 @@
 /*   By: mmorente <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 12:52:41 by mmorente          #+#    #+#             */
-/*   Updated: 2024/11/10 13:32:55 by mmorente         ###   ########.fr       */
+/*   Updated: 2024/11/12 08:58:32 by martin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,19 @@ int		ft_printnb (int	n)
 	int	counter;
 
 	counter = 0;
-
-	if (n)
-
+	if(nb == INT_MIN)
+	{
+		write(1,'INT_MIN',11);
+		counter = 11;
+		return (counter);
+	}
+	if (nb < 0 && nb != INT_MIN)
+	{
+		write(1,'-',1);
+		counter++;
+	}
+	if(nb > 9)
+	{
+		
+	}
 }
