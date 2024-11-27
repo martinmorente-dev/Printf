ec3b12c3c79747cc00c1d7f976cf7b6fa6953b5f
martinmorente-Dev <martin.dev45@gmail.com>
2024-11-27 11:17:45 +0100

    Update

--- prueba.c
+++ prueba.c
@@ -2,87 +2,85 @@
 #include <limits.h>
 #include <stdarg.h>
 
-int     ft_printnb (int n,int counter)
+int     ft_printnb(int n, int counter)
 {
-        if(n == INT_MIN)
+    if (n == INT_MIN)
+    {
+        write(1, "-2147483648", 11);
+        counter = 11;
+        return (counter);
+    }
+    if (n < 0 && n != INT_MIN)
+    {
+        write(1, "-", 1);
+        counter++;
+        n = -n;
+    }
+    if (n < 10)
+    {
+        n = n + '0';
+        write(1, &n, 1);
+        counter++;
+    }
+    else
+    {
+        counter = ft_printnb(n / 10, counter);
+        n = (n % 10) + '0';
+        write(1, &n, 1);
+        counter++;
+    }
+    return (counter);
+}
+
+int     format(va_list args, const char *str, int counter)
+{
+    if (*str == 'd' || *str == 'i')
+    {
+        counter = ft_printnb(va_arg(args, int), counter);
+    }
+    return (counter);
+}
+
+int     ft_printstr(char *str, int counter)
+{
+    int i = 0;
+    while (str[i] != '%' && str[i] != '\0')
+    {
+        write(1, &str[i], 1);
+        i++;
+        counter++;
+    }
+    return counter;
+}
+
+int     ft_printf(const char *str, ...)
+{
+    va_list args;
+    int checker = 0;
+
+    if (!str)
+        return (0);
+
+    va_start(args, str);
+    while (*str)
+    {
+        if (*str == '%')
         {
-                write(1,"-2147483648",11);
-                counter = 11;
-                return (counter);
-        }
-        if (n < 0 && n != INT_MIN)
-        {
-                write(1,"-",1);
-                counter++;
-                n = -n;
-        }
-        if (n < 10)
-        {
-                n = n + '0';
-                write(1,&n,1);
-                counter++;
+            str++;
+            checker = format(args, str, checker);
         }
         else
         {
-                counter = ft_printnb((n / 10), counter);
-                n = (n % 10) + '0';
-                write(1,&n,1);
-                counter++;
+            checker = ft_printstr((char *)str, checker);
         }
-        return (counter);
+        str++;
+    }
+    va_end(args);
+    return (checker);
 }
 
-int    format(va_list args, char *str)
+int main(void)
 {
-        int     counter;
-
-        counter = 0;
-       	if(*str == 'd' || *str == 'i')
-				ft_printnb((va_arg(args, int)),counter);
-		return (counter);
- //Print a decimal number on base 10
-}
-
-int	ft_printstr(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		write(1,&str[i],1);
-		i++;
-	}
-	return(i);
-}
-
-int     ft_printf(char const *str, ...)
-{
-        va_list			args;
-        int             checker;
-
-        if (!str)
-                return (0);
-        checker = 0;
-        va_start(args,str);
-        while (*str)
-        {
-             if (*str == '%')
-            {
-                str++;
-                checker += format(args, (char *)str);
-            }
-			else
-				checker += ft_printstr((char *)str);		
-			str++;
-		}
-		va_end(args);
-		return(checker);
-}
-
-int	main(void)
-{
-	ft_printf("Hola Luis tu edad es %d",108);
-
-	return(0);
+    ft_printf("Hola Luis tu edad es %d", 108);
+    return (0);
 }
